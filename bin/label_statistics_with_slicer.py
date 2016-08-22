b6c17b45e0dbff474543bd9dd4e40524e2a85dd1
zhangfanmark <zhangfanmark@gmail.com>
2016-08-22 18:15:02 -0400

    Add index of the input regions

--- bin/label_statistics_with_slicer.py
+++ bin/label_statistics_with_slicer.py
@@ -5,25 +5,25 @@ import slicer
 import vtk
 
 parser = argparse.ArgumentParser(
-	description="Compute volume statistics with Slicer.",
-	epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu")
+    description="Compute volume statistics with Slicer.",
+    epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu")
 parser.add_argument("-v", "--version",
-	action="version", default=argparse.SUPPRESS,
-	version='1.0',
-	help="Show program's version number and exit")
+    action="version", default=argparse.SUPPRESS,
+    version='1.0',
+    help="Show program's version number and exit")
 
 parser.add_argument(
-	'inputLabelmapDirectory',
-	help='')
+    'inputLabelmapDirectory',
+    help='')
 parser.add_argument(
-	'inputVolumeDirectory',
-	help='')
+    'inputVolumeDirectory',
+    help='')
 parser.add_argument(
-	'outputFile',
-	help='')
+    'outputFile',
+    help='')
 parser.add_argument(
-	'region', type=int,
-	help='')
+    'region', type=int,
+    help='')
 
 args = parser.parse_args()
 
@@ -34,86 +34,91 @@ region_index = args.region
 
 def list_files(input_dir):
 
-	mask = "{0}/*".format(input_dir)
-	fnames = glob.glob(mask)
-	fnames = sorted(fnames)
+    mask = "{0}/*".format(input_dir)
+    fnames = glob.glob(mask)
+    fnames = sorted(fnames)
 
-	return fnames
+    return fnames
 
 labelmap_paths = list_files(input_labelmap_dir)
 volume_paths = list_files(input_volume_dir)
 
+keys = ("Subject", "Index", "Count", "Volume mm^3", "Volume cc", "Min", "Max", "Mean", "StdDev")
+labelStats = {}
+labelStats['Labels'] = []
+sub_count = 0
 for labelmap_path, volume_path in zip(labelmap_paths, volume_paths):
-	print "======", labelmap_path, ' <TO> ', volume_path
+    print "======", labelmap_path, ' <TO> ', volume_path
 
-	check_load, labelmap_node = slicer.util.loadLabelVolume(labelmap_path, {}, True)
-	check_load, volume_node = slicer.util.loadVolume(volume_path, {}, True)
+    check_load, labelmap_node = slicer.util.loadLabelVolume(labelmap_path, {}, True)
+    check_load, volume_node = slicer.util.loadVolume(volume_path, {}, True)
 
-	## logic as in Slicer LabelStatistics.py
-	keys = ("Subject", "Count", "Volume mm^3", "Volume cc", "Min", "Max", "Mean", "StdDev")
-	cubicMMPerVoxel = reduce(lambda x,y: x*y, labelmap_node.GetSpacing())
-	ccPerCubicMM = 0.001
+    ## logic as in Slicer LabelStatistics.py
+    cubicMMPerVoxel = reduce(lambda x,y: x*y, labelmap_node.GetSpacing())
+    ccPerCubicMM = 0.001
 
-	labelStats = {}
-	labelStats['Labels'] = []
+    stataccum = vtk.vtkImageAccumulate()
+    stataccum.SetInputConnection(labelmap_node.GetImageDataConnection())
+    stataccum.Update()
+    lo = int(stataccum.GetMin()[0])
+    hi = int(stataccum.GetMax()[0])
 
-	stataccum = vtk.vtkImageAccumulate()
-	stataccum.SetInputConnection(labelmap_node.GetImageDataConnection())
-	stataccum.Update()
-	lo = int(stataccum.GetMin()[0])
-	hi = int(stataccum.GetMax()[0])
+    if region_index > hi or region_index < lo:
+        print "Error: Input region index ", region_index, "does not exist."
 
-	if region_index > hi or region_index < lo:
-		print "Error: Input region index ", region_index, "does not exist."
+    i = region_index
 
-	i = region_index
+    thresholder = vtk.vtkImageThreshold()
+    thresholder.SetInputConnection(labelmap_node.GetImageDataConnection())
+    thresholder.SetInValue(1)
+    thresholder.SetOutValue(0)
+    thresholder.ReplaceOutOn()
+    thresholder.ThresholdBetween(i,i)
+    thresholder.SetOutputScalarType(volume_node.GetImageData().GetScalarType())
+    thresholder.Update()
 
-	thresholder = vtk.vtkImageThreshold()
-	thresholder.SetInputConnection(labelmap_node.GetImageDataConnection())
-	thresholder.SetInValue(1)
-	thresholder.SetOutValue(0)
-	thresholder.ReplaceOutOn()
-	thresholder.ThresholdBetween(i,i)
-	thresholder.SetOutputScalarType(volume_node.GetImageData().GetScalarType())
-	thresholder.Update()
+    #  use vtk's statistics class with the binary labelmap as a stencil
+    stencil = vtk.vtkImageToImageStencil()
+    stencil.SetInputConnection(thresholder.GetOutputPort())
+    stencil.ThresholdBetween(1, 1)
 
-	#  use vtk's statistics class with the binary labelmap as a stencil
-	stencil = vtk.vtkImageToImageStencil()
-	stencil.SetInputConnection(thresholder.GetOutputPort())
-	stencil.ThresholdBetween(1, 1)
+    stat1 = vtk.vtkImageAccumulate()
+    stat1.SetInputConnection(volume_node.GetImageDataConnection())
+    stencil.Update()
+    stat1.SetStencilData(stencil.GetOutput())
 
-	stat1 = vtk.vtkImageAccumulate()
-	stat1.SetInputConnection(volume_node.GetImageDataConnection())
-	stencil.Update()
-	stat1.SetStencilData(stencil.GetOutput())
+    stat1.Update()
 
-	stat1.Update()
+    if stat1.GetVoxelCount() > 0:
+        # add an entry to the LabelStats list
+        labelStats["Labels"].append(sub_count)
+        labelStats[sub_count,"Subject"] = os.path.split(volume_path)[1]
+        labelStats[sub_count,"Index"] = i
+        labelStats[sub_count,"Count"] = stat1.GetVoxelCount()
+        labelStats[sub_count,"Volume mm^3"] = labelStats[sub_count,"Count"] * cubicMMPerVoxel
+        labelStats[sub_count,"Volume cc"] = labelStats[sub_count,"Volume mm^3"] * ccPerCubicMM
+        labelStats[sub_count,"Min"] = stat1.GetMin()[0]
+        labelStats[sub_count,"Max"] = stat1.GetMax()[0]
+        labelStats[sub_count,"Mean"] = stat1.GetMean()[0]
+        labelStats[sub_count,"StdDev"] = stat1.GetStandardDeviation()[0]
+        sub_count = sub_count + 1
 
-	if stat1.GetVoxelCount() > 0:
-		# add an entry to the LabelStats list
-		labelStats["Labels"].append(i)
-		labelStats[i,"Subject"] = os.path.split(volume_path)[1]
-		labelStats[i,"Count"] = stat1.GetVoxelCount()
-		labelStats[i,"Volume mm^3"] = labelStats[i,"Count"] * cubicMMPerVoxel
-		labelStats[i,"Volume cc"] = labelStats[i,"Volume mm^3"] * ccPerCubicMM
-		labelStats[i,"Min"] = stat1.GetMin()[0]
-		labelStats[i,"Max"] = stat1.GetMax()[0]
-		labelStats[i,"Mean"] = stat1.GetMean()[0]
-		labelStats[i,"StdDev"] = stat1.GetStandardDeviation()[0]
+    slicer.mrmlScene.RemoveNode(labelmap_node)
+    slicer.mrmlScene.RemoveNode(volume_node)
 
 # output csv
 csv = ""
 header = ""
 for k in keys[:-1]:
-	header += "\"%s\"" % k + ","
+    header += "\"%s\"" % k + ","
 header += "\"%s\"" % keys[-1] + "\n"
 csv = header
-for i in labelStats["Labels"]:
-	line = ""
-	for k in keys[:-1]:
-		line += str(labelStats[i,k]) + ","
-	line += str(labelStats[i,keys[-1]]) + "\n"
-	csv += line
+for sub_count in labelStats["Labels"]:
+    line = ""
+    for k in keys[:-1]:
+        line += str(labelStats[sub_count,k]) + ","
+    line += str(labelStats[sub_count,keys[-1]]) + "\n"
+    csv += line
 
 fp = open(output_csv, "w")
 fp.write(csv)
--- bin/wm_label_statistics_of_volume.py
+++ bin/wm_label_statistics_of_volume.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(
-	description="Compute volume statistics with Slicer.",
+	description="Compute label statistics of the input volume given a labelmap with Slicer.",
 	epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu")
 parser.add_argument("-v", "--version",
 	action="version", default=argparse.SUPPRESS,
@@ -11,19 +11,19 @@ parser.add_argument("-v", "--version",
 
 parser.add_argument(
 	'inputLabelmapDirectory',
-	help='Directory of input labelmaps (e.g., freesurfer parcellation). The script will calculate label statistics of the input volume (as in LabelStatistics module in 3D Slicer) given each region of the input label map.')
+	help='Directory of input labelmaps (e.g., freesurfer parcellation). The script will calculate label statistics of the input volume (as in LabelStatistics module in 3D Slicer) given one region of the input label map.')
 parser.add_argument(
 	'inputVolumeDirectory',
-	help='Directory of input image volume (e.g., T1, T2 or B0 image). Note: Image volume and the corresponding labelmap must have same dimensions.')
+	help='Directory of input image volumes (e.g., T1, T2 or B0 image). Note: Image volume and the corresponding labelmap must have same dimensions.')
 parser.add_argument(
 	'outputFile',
-	help='A CVS file that contains the statistics.')
+	help='A CVS file that contains the output statistics.')
 parser.add_argument(
 	'Slicer',
 	help='Path of 3D Slicer.')
 parser.add_argument(
 	'-r',  dest='region', type=int,
-	help='A list of selected regions. If not given, all regions will be included.')
+	help='Region index.')
 
 args = parser.parse_args()
 
